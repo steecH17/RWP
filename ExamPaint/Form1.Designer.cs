be17635715e1fa8380133a6d6d3068ec1ffc6289
vniki <vniki@STEECH>
2024-01-12 01:16:12 +0300

    Добавьте файлы проекта.

---
+++ ExamPaint/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ExamPaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel = new System.Windows.Forms.Panel();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonColor = new System.Windows.Forms.Button();
+            this.textBoxSpeed = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(12, 12);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(799, 413);
+            this.panel.TabIndex = 0;
+            this.panel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_MouseDown);
+            this.panel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel_MouseMove);
+            this.panel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel_MouseUp);
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(589, 21);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(222, 152);
+            this.buttonRun.TabIndex = 1;
+            this.buttonRun.Text = "Воспроизвести";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonClear);
+            this.groupBox1.Controls.Add(this.buttonColor);
+            this.groupBox1.Controls.Add(this.textBoxSpeed);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.buttonRun);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(0, 431);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(823, 185);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Управление";
+            // 
+            // buttonColor
+            // 
+            this.buttonColor.Location = new System.Drawing.Point(293, 37);
+            this.buttonColor.Name = "buttonColor";
+            this.buttonColor.Size = new System.Drawing.Size(84, 41);
+            this.buttonColor.TabIndex = 5;
+            this.buttonColor.UseVisualStyleBackColor = true;
+            this.buttonColor.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // textBoxSpeed
+            // 
+            this.textBoxSpeed.Location = new System.Drawing.Point(277, 122);
+            this.textBoxSpeed.Name = "textBoxSpeed";
+            this.textBoxSpeed.Size = new System.Drawing.Size(100, 22);
+            this.textBoxSpeed.TabIndex = 4;
+            this.textBoxSpeed.TextChanged += new System.EventHandler(this.textBoxSpeed_TextChanged);
+            this.textBoxSpeed.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxSpeed_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Скорость воспроизведения";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Выбор цвета для вопсроизведения";
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(454, 37);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(104, 41);
+            this.buttonClear.TabIndex = 6;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 616);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxSpeed;
+        private System.Windows.Forms.Button buttonColor;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ ExamPaint/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamPaint
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        List<Point> points = new List<Point>();
+        bool isDraw = false;//режим когда зажата левая кнопка мыши и мы двигаем мышкой
+        bool replayMode = false;//режим воспроизведения
+        Color brushColor = Color.Red;//цвет кисти
+        int countPoints = 0;//количество уже отрисованных точек(он нам понадобиться при воспроиздвении)
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(panel.ClientSize.Width, panel.ClientSize.Height);
+            buttonRun.Enabled = false;//блокируем кнопку
+            buttonColor.BackColor = brushColor;//закрышиваем ее
+            timer.Interval = 100;
+            textBoxSpeed.Text = "1";
+            typeof(Panel).InvokeMember("DoubleBuffered", BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic, null, panel, new object[] { true });
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            bmp = new Bitmap(panel.ClientSize.Width, panel.ClientSize.Height);
+            Graphics graphics = Graphics.FromImage(bmp);
+            Pen pen = new Pen(brushColor, 5);
+            if(points.Count>1 && !replayMode)//когда не включено воспроизведение и количество точек больше 1, чтобы можно было соединить хотя бы 2 точки
+            {
+                graphics.DrawLines(pen, points.ToArray());//рисуем линию
+            }
+            else if(replayMode && countPoints>0)//режим воспроизведения. На каждом тике таймера будем отрисовывать какое то количество линий кол-во которых будет увеличиваться по времени
+            {
+                for(int i =0; i<countPoints-1; i++)
+                {
+                    graphics.DrawLine(pen, points[i], points[i+1]);//соедением линии
+                }
+            }
+            e.Graphics.DrawImageUnscaled(bmp, 0, 0);
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            //кнопка воспроизведения
+            timer.Start();
+            replayMode = true;
+        }
+
+        private void panel_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDraw = true;
+        }
+
+        private void panel_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDraw = false;
+            buttonRun.Enabled = true;
+        }
+
+        private void panel_MouseMove(object sender, MouseEventArgs e)
+        {
+            //если зажата левая кнопка мыши значить isDraw=true тогда просто кидаем в список текущие координаты мышки и отрисываем
+            if(isDraw)
+            {
+                points.Add(e.Location);
+            }
+            panel.Invalidate();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            countPoints++;// на каждом тике добавляем точку для отрисовки
+            if(countPoints == points.Count)//завершаем работу таймера если достигли максимального кол-ва точек(т.е все нарисовали)
+            {
+                timer.Stop();
+                countPoints = 0;
+                replayMode = false;
+            }
+            panel.Invalidate();
+        }
+
+        private void textBoxSpeed_TextChanged(object sender, EventArgs e)
+        {
+            int speed;//сюда получим значение лежащее в текс боксе
+            if(int.TryParse(textBoxSpeed.Text, out int result))
+            {
+                speed = result;
+                if(speed > 100 || speed == 0)//ограничения, короче у меня 100 это Interval = 1 милисекунда, 1 это Interval = 100 милисек
+                {
+                    //интервал это как часто срабатыввает это событие то есть гапример интервал 1000 милисекунд это значит срабатывает раз в секунду
+                    MessageBox.Show("Максимальная скорость воспроизведения 100. Минимальная 1.");
+                    timer.Interval = 100;
+                    textBoxSpeed.Text = "1";//просто заносим в текс бокс свой текст
+                }
+                else if(speed == 100)
+                {
+                    timer.Interval = 1;
+                }
+                else timer.Interval = 100 - speed;//устанавливаем интервал в зависимости от скорости
+
+            }
+        }
+
+        private void buttonColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();//диалог выбора цвета
+            if(colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                brushColor = colorDialog.Color;//меняем цвет кисти
+                buttonColor.BackColor = brushColor;//меняем цвет кнопки
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            //кнопка очищение поверхности рисования
+            points.Clear();
+            countPoints = 0;
+            buttonRun.Enabled = false;
+            panel.Invalidate();
+        }
+
+        private void textBoxSpeed_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //это просто ограничения на ввод, то есть можем вводить только 0-9 и ентер и удалить
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
